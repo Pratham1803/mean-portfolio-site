20ec1ff943dd7ae2705a9b8e916b83101761c9ad
pratham1803 <114798779+Pratham1803@users.noreply.github.com>
2026-01-07 17:53:55 +0530

    minor change

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://mean-portfolio-site.onrender.com/portfolio/api/v1/";
-const BASE_URL = "http://localhost:2000/portfolio/api/v1/";
+const BASE_URL = "https://mean-portfolio-site.onrender.com/portfolio/api/v1/";
+// const BASE_URL = "http://localhost:2000/portfolio/api/v1/";
 
 var app = angular.module("portfolioApp", []);
