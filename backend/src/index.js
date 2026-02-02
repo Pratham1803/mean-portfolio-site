69a8e14733bdcf9beab1fb0a9ee8d89fb269f33c
pratham1803 <114798779+Pratham1803@users.noreply.github.com>
2026-02-02 16:33:17 +0530

    vercel build changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,12 @@ const connectDb = require("./db");
 connectDb()
   .then(() => {
     const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);      
-    });
+
+    if (process.env.NODE_ENV === "DEV") {
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    }
   })
   .catch((error) => {
     console.log(`Failed to connect to database: ${error.message}`);
